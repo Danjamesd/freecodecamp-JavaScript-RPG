437e1b24d667377a881b3b25ccb6628250893db1
Danjamesd <126273189+Danjamesd@users.noreply.github.com>
2023-04-03 11:24:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// initialised int variables
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+// declared variables
+let fighting;
+let monsterHealth;
+// initialised string variables
+let inventory = ["stick"]; // arrays do not need to be filled in order to be parsed. 
+
